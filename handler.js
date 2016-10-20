56418965712924d9d4d17160dad512dd99cf4c1c
Sterling Camden <sterling@relateiq.com>
2016-10-19 17:43:02 -0700

    finally got basic endpoint working, now try to connect to rds

--- handler.js
+++ handler.js
@@ -1,12 +1,32 @@
 'use strict';
 
+// var sequelize = new require('sequelize')('database', 'username', 'password', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   },
+
+// });
+
+var sequelize = new Sequelize("postgres://scamden:E6Cb.M4QBWW*LT@siqstorydb.cuwyz56hhvzk.us-east-1.rds.amazonaws.com:5432");
+
 module.exports.hello = (event, context, callback) => {
+  let result;
+  sequelize
+    .authenticate()
+    .then(function (err) {
+      result = 'Connection has been established successfully.';
+    })
+    .catch(function (err) {
+      result = 'Unable to connect to the database:';
+    });
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: result,
   };
 
   callback(null, response);
