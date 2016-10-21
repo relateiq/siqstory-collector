ec012f1c8c60e63d8df890afff895a2562b67e27
Sterling Camden <sterling@relateiq.com>
2016-10-20 22:28:18 -0700

    circle ci is amazing

--- handler.js
+++ handler.js
@@ -11,11 +11,41 @@
 //   },
 
 // });
+console.log('require sequelize');
+var Sequelize = require('sequelize');
+// postgres: //[db_username]:[db_password]@[hostname]:[port]/[db_name]
+console.log('create sequelize instance');
+var sequelize = new Sequelize("postgres://scamden:E6Cb.M4QBWW*LT@siqstorydb-2.cuwyz56hhvzk.us-east-1.rds.amazonaws.com:5432/siqstorydb");
+// console.log(sequelize);
 
-var sequelize = new Sequelize("postgres://scamden:E6Cb.M4QBWW*LT@siqstorydb.cuwyz56hhvzk.us-east-1.rds.amazonaws.com:5432");
 
 module.exports.hello = (event, context, callback) => {
-  let result;
+  let result = 'default result';
+  // console.log('sequelize define');
+  // var User = sequelize.define('user', {
+  //   firstName: {
+  //     type: Sequelize.STRING
+  //   },
+  //   lastName: {
+  //     type: Sequelize.STRING
+  //   }
+  // });
+
+  // // force: true will drop the table if it already exists
+  // User.sync({
+  //   force: true
+  // }).then(function () {
+  //   // Table created
+  //   return User.create({
+  //     firstName: 'John',
+  //     lastName: 'Hancock'
+  //   }).then(function () {
+  //     User.findAll().then(function (users) {
+  //       callback(null, users);
+  //     })
+  //   });
+  // });
+
   sequelize
     .authenticate()
     .then(function (err) {
@@ -23,14 +53,23 @@ module.exports.hello = (event, context, callback) => {
     })
     .catch(function (err) {
       result = 'Unable to connect to the database:';
-    });
-  const response = {
-    statusCode: 200,
-    body: result,
-  };
+    }).finally(function () {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: result,
+          input: event,
+        }),
+      };
 
-  callback(null, response);
+      callback(null, response);
+    });
+
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+// module.exports.hello(null, null, function (c, r) {
+//   console.log('callback', r)
+// });
